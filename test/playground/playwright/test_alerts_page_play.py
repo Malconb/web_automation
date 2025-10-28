3eaec31594c16f619c15705c2dfbcc94d62a44eb
miguel alcon <miguelangel.alconb@gmail.com>
2025-10-28 21:11:39 +0000

    reverting changes

--- test/playground/playwright/test_alerts_page_play.py
+++ test/playground/playwright/test_alerts_page_play.py
@@ -1,6 +1,6 @@
 import pytest
 import re 
-from playwright.sync_api import Page, sync_playwright
+from playwright.sync_api import Page
 from pages.playground.playwright.alerts_page_play import AlertsPagePlay
 from dotenv import load_dotenv
 import os
@@ -21,13 +21,7 @@ password2 = os.getenv("password2")
 @pytest.mark.smoke
 @pytest.mark.sample_app_page
 class TestAlertsPlay:
-    def setup(self):
-        self.browser = sync_playwright.chromium.launch(headless=False)
-        self.page = self.browser.new_page()
-
-    def teardown(self):
-        self.browser.close()
-
+ 
     @pytest.mark.alert_success
     def test_alerts_success(self, page: Page, log_test_name) -> None:
         alerts_page = AlertsPagePlay(page)
